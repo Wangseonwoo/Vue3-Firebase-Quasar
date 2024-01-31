9cfcd877de1a8a049eb57c5eddca1c3b809a9bfb
Wangseonwoo <dmb05007@naver.com>
2024-01-31 16:06:43 +0900

    postComponent

---
+++ src/components/StickySideBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <aside class="sticku-side-bar">
+    <slot></slot>
+  </aside>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.sticku-side-bar {
+  position: sticky;
+  top: 60px;
+  z-index: 2;
+  height: fit-content !important;
+}
+</style>
