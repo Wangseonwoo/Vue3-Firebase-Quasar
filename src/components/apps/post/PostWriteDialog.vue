69ad2a58e45dca0487cd8ab46e3fe0bb3a225621
Wangseonwoo <dmb05007@naver.com>
2024-02-04 14:16:06 +0900

    post-form

--- src/components/apps/post/PostWriteDialog.vue
+++ src/components/apps/post/PostWriteDialog.vue
@@ -6,40 +6,11 @@
         <q-btn v-close-popup flat round dense icon="close" />
       </q-toolbar>
       <q-separator />
-      <q-form class="q-pa-md q-gutter-y-sm">
-        <q-input v-model="form.title" outlined placeholder="제목" />
-        <q-select outlined v-model="form.category" :options="categories">
-          <template v-if="!form.category" #selected>
-            <span class="text-grey-7">카테고리를 입력하세요</span>
-          </template>
-        </q-select>
-        <q-input
-          v-model="form.content"
-          type="textarea"
-          outlined
-          placeholder="내용을 작성해주세요"
-        />
-        <q-input
-          v-model="tagField"
-          outlined
-          placeholder="태그를 입력해주세요"
-          prefix="#"
-        />
-        <q-chip outline dense color="teal" removable @remove="removeTag"
-          >vuejs</q-chip
-        >
-      </q-form>
-      <q-separator />
-      <q-card-actions align="right">
-        <q-btn flat label="취소하기" v-close-popup />
-        <q-btn
-          type="submit"
-          flat
-          label="저장하기"
-          color="primary"
-          v-close-popup
-        />
-      </q-card-actions>
+      <PostForm
+        v-model:title="form.title"
+        v-model:category="form.category"
+        v-model:content="form.content"
+      />
     </q-card>
   </q-dialog>
 </template>
@@ -52,17 +23,14 @@ const getInitialForm = () => ({
   tags: [],
 });
 </script>
+
 <script setup>
 import { ref } from 'vue';
-import { getCategories } from 'src/services/category';
-const categories = getCategories();
+import PostForm from './PostForm.vue';
 
 const form = ref(getInitialForm());
-const tagField = ref('');
-const removeTag = () => {};
 const onHide = () => {
   form.value = getInitialForm();
-  tagField.value = '';
 };
 </script>
 
