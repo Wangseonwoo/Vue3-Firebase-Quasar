7a832be2c424972ea180bad19b46547ff50191ae
Wangseonwoo <dmb05007@naver.com>
2024-02-07 17:05:44 +0900

    firebase-login-out

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,12 +3,10 @@ import { boot } from 'quasar/wrappers';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAnalytics } from 'firebase/analytics';
-import { getAuth } from 'firebase/auth';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { useAuthStore } from 'src/stores/auth';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyBnqn2jrIHLdmqbxPHUcGjUI58UIfOvjEU',
   authDomain: 'wang-vue3-firebase-app-7226f.firebaseapp.com',
@@ -27,5 +25,9 @@ const auth = getAuth(app);
 export { auth };
 
 export default boot(async (/* { app, router, ... } */) => {
-  // something to do
+  const authStore = useAuthStore();
+  onAuthStateChanged(auth, user => {
+    console.log('### user: ', user);
+    authStore.setUser(user);
+  });
 });
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -35,22 +35,23 @@
         />
         <q-separator class="q-my-md q-mr-md" vertical />
         <q-btn
+          v-if="!authStore.isAuthenticated"
           unelevated
           rounded
           color="primary"
           label="로그인 / 회원가입"
           @click="openAuthDialog"
         />
-        <q-btn round flat>
+        <q-btn v-if="authStore.isAuthenticated" round flat>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/img/avatar.png" />
+            <img :src="authStore.user.photoURL" />
           </q-avatar>
           <q-menu>
             <q-list style="min-width: 100px">
               <q-item clickable v-close-popup to="/mypage/profile">
                 <q-item-section>프로필</q-item-section>
               </q-item>
-              <q-item clickable v-close-popup>
+              <q-item clickable v-close-popup @click="handleLogout">
                 <q-item-section>로그아웃</q-item-section>
               </q-item>
             </q-list>
@@ -69,7 +70,12 @@
 <script setup>
 import { computed, ref } from 'vue';
 import { useRoute } from 'vue-router';
+import { useAuthStore } from 'src/stores/auth';
+import { logout } from 'src/services/auth';
+
 import AuthDialog from 'src/components/auth/AuthDialog.vue';
+
+const authStore = useAuthStore();
 
 const route = useRoute();
 // console.dir(route);
@@ -82,5 +88,8 @@ const pageContainerStyles = computed(() => ({
 const authDialog = ref(false);
 const openAuthDialog = () => {
   authDialog.value = true;
+};
+const handleLogout = async () => {
+  await logout();
 };
 </script>
